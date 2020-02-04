056843537225bc7c894103de4cf7d4bc49d1d7c7
wx0000 <w.szaron@gmail.com>
2020-02-04 18:18:02 +0100

    Updates

---
+++ precache-manifest.4341e66d96638476587708d0c4eab253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c78a89fa5398222443d4171c25374a3",
+    "url": "/MonsterHunterz/index.html"
+  },
+  {
+    "revision": "3f03f9f16f81c8f15465",
+    "url": "/MonsterHunterz/static/css/main.2160a16b.chunk.css"
+  },
+  {
+    "revision": "80c19bc18df587c05690",
+    "url": "/MonsterHunterz/static/js/2.0e3f8dd3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterHunterz/static/js/2.0e3f8dd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f03f9f16f81c8f15465",
+    "url": "/MonsterHunterz/static/js/main.0bad8834.chunk.js"
+  },
+  {
+    "revision": "ad123352bc591849064c",
+    "url": "/MonsterHunterz/static/js/runtime-main.27cc7b6f.js"
+  }
+]);
